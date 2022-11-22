d40aed3e0eeca3fc2b6217b0daf4ff5631d7e6c3
Ayush singh <78909117+Ayushsingh07@users.noreply.github.com>
2022-11-22 21:50:35 +0530

    Added solution - LeetHub

---
+++ Magic Triplets - GFG/magic-triplets.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+int countTriplets(vector<int>nums){
+	    
+	    int final_count=0;
+	    
+	    for(int i=1;i<nums.size()-1;i++)
+	    {
+	        int j = i-1,flag=0,count_small=0,k=i+1,count_big=0;
+	        
+	        int val = nums[i];
+	        
+		//count of smaller numbers to left
+	        while(j>=0)
+	        {
+	            if(nums[j]<val)
+	            {
+                         count_small++;
+	            }
+	            j--;
+	        }
+	        
+		//count of bigger numbers to right
+	        while(k<nums.size())
+	        {
+	            if(nums[k]>val)
+	            {
+	                count_big++;
+	            }
+	            k++;
+	        }
+					
+		//add there multiplication to add into count
+	        final_count+=(count_small*count_big);
+	    }
+	    
+	    return final_count;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)cin >> nums[i];
+		Solution ob;
+		int ans = ob.countTriplets(nums);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
